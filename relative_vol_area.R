7ce44ae9da73e212b0a288c577fba863679a82aa
Ursula Laa <uschi.laa@gmx.at>
2019-08-28 16:14:57 +1000

    Rewriting of the introduction.

--- relative_vol_area.R
+++ relative_vol_area.R
@@ -28,13 +28,27 @@ r <- 1
 
 ggplot(data.frame(x=c(0.01, 0.5)), aes(x=x)) +
   stat_function(fun=relVol(3, r), geom="line", color="black") +
+  stat_function(fun=relVol(4, r), geom="line", color="purple") +
   stat_function(fun=relVol(6, r), geom="line", color="blue") +
   stat_function(fun=relVol(10, r), geom="line", color="green") +
   stat_function(fun=relVol(20, r), geom="line", color="red") +
   stat_function(fun=relArea(3, r), geom="line", color="black", linetype="dashed") +
+  stat_function(fun=relArea(4, r), geom="line", color="purple", linetype="dashed") +
   stat_function(fun=relArea(6, r), geom="line", color="blue", linetype="dashed") +
   stat_function(fun=relArea(10, r), geom="line", color="green", linetype="dashed") +
   stat_function(fun=relArea(20, r), geom="line", color="red", linetype="dashed") +
+  xlab("h") +
+  ylab("relative volume/area")
+
+ggplot(data.frame(x=c(0.01, 0.2)), aes(x=x)) +
+  stat_function(fun=relVol(3, r), geom="line", color="black") +
+  stat_function(fun=relVol(4, r), geom="line", color="blue") +
+  stat_function(fun=relVol(5, r), geom="line", color="green") +
+  stat_function(fun=relVol(6, r), geom="line", color="red") +
+  stat_function(fun=relArea(3, r), geom="line", color="black", linetype="dashed") +
+  stat_function(fun=relArea(4, r), geom="line", color="blue", linetype="dashed") +
+  stat_function(fun=relArea(5, r), geom="line", color="green", linetype="dashed") +
+  stat_function(fun=relArea(6, r), geom="line", color="red", linetype="dashed") +
   xlab("h") +
   ylab("relative volume/area")
 
